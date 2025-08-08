69e26951a81284459262ea55c15f078f68c487c2
Ramik Mustafayev <ramikmustafayev@gmail.com>
2025-08-08 14:46:19 +0400

    fix bugs

--- tasks/reset_column.py
+++
@@ -1,7 +0,0 @@
-from database.repo.requests import RequestsRepo
-
-async def reset_column(session_pool):
-    async with session_pool() as session:
-            repo=RequestsRepo(session)
-            pump_tokens=repo.pump_tokens
-            await pump_tokens.update({},{'sygnal_per_day':0})
